1f36200e1ad5ba52826b25d9112e5a7eb06f62fd
Vitor Henrique <vitorpc4@hotmail.com>
2022-07-24 17:04:05 -0300

    Alterar get para dia e mês
    Agora o get dos meses e dos anos são feito de forma automática baseado na localização do computador.

--- Frontend/src/pages/LaunchingTargets/launchingClothes.vue
+++ Frontend/src/pages/LaunchingTargets/launchingClothes.vue
@@ -2,16 +2,18 @@
   <div class="q-pa-md">
     <div class="row justify-end q-gutter-sm">
       <div class="col-1 q-col-gutter-md">
-        <q-select borderless v-model="selectMonth" :options="optionsMonths" label="Meses">
+        <q-select borderless v-model="selectMonth" @popup-hide="refreshDaysMonths" :options="optionsMonths"
+          label="Meses">
         </q-select>
       </div>
       <div class="col-1 q-col-gutter-md">
-        <q-select borderless v-model="selectYear" @popup-hide="filterListByYear" :options="optionsYear" label="Ano">
+        <q-select borderless v-model="selectYear" :options="optionsYear" label="Ano">
         </q-select>
       </div>
     </div>
     <div v-for="(days, index) in numberOfMonth" :key="index">
-      <q-expansion-item icon="fa-solid fa-calendar-day" :label="selectMonth.toUpperCase()" :caption="days.toString()">
+      <q-expansion-item icon="fa-solid fa-calendar-day" :label="selectMonth.toUpperCase() + ' ' + days"
+        :caption="getNameDay(selectYear, selectMonth, days)">
         <q-card>
           <q-card-section>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
@@ -20,35 +22,19 @@
           </q-card-section>
         </q-card>
       </q-expansion-item>
-      <!-- <q-expansion-item caption="`${days}`">
-
-        <template v-slot:header>
-          <q-item-section avatar>
-            <q-avatar icon="fa-solid fa-calendar-day" color="primary" text-color="white" />
-          </q-item-section>
-          <q-item-section>
-            {{ days }}
-          </q-item-section>
-
-        </template>
-
-
-
-      </q-expansion-item> -->
     </div>
   </div>
 </template>
 <script lang="ts">
 
 import { useTargetStore } from 'src/stores/TargetStore'
-import { computed, defineComponent, onMounted, ref } from 'vue'
+import { computed, defineComponent, onMounted, Ref, ref } from 'vue'
 
 
 export default defineComponent({
   setup() {
     const useTarget = useTargetStore()
-    const optionsMonths = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
-    let daysArray = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
+    const optionsMonths: Ref<string[]> = ref([])
     const actualDate = new Date();
     let selectMonth = ref('')
     let selectYear = ref('')
@@ -60,36 +46,53 @@ export default defineComponent({
     let getNumberOfMonth: number
     let numberOfMonth = ref(0)
 
-    const getDayInMonths = (year: number, month: number) => {
+
+    const getNameofMonth = () => {
+      let d = new Date(2000, 0); // January
+      for (let i = 0; i < 12; i++) {
+        optionsMonths.value.push(d.toLocaleString('default', { month: 'long' }))
+        d.setMonth(i + 1);
+      }
+      return optionsMonths;
+    }
+
+    const getDaysInMonths = (year: number, month: number) => {
       return new Date(year, month, 0).getDate();
     }
 
-    const getNameDay = () => {
-      let day = new Date('2012, 10, 0').getDate()
-      let dayName = daysArray[day];
-      console.log(dayName)
+    const getNameDay = (year: string, month: string, dayNumber: number) => {
+      let monthIndex = optionsMonths.value.indexOf(selectMonth.value)
+      let day = new Date(`${year}-${monthIndex + 1}-${dayNumber}`).toLocaleString('default', { weekday: 'long' })
+      let dayName = day;
+
+      return dayName
     }
 
-
+    const refreshDaysMonths = () => {
+      getNumberOfMonth = optionsMonths.value.indexOf(selectMonth.value)
+      numberOfMonth.value = getDaysInMonths(actualYear, getNumberOfMonth + 1)
+    }
 
 
     onMounted(() => {
       selectMonth.value = actualMonth
       selectYear.value = actualYear.toString()
       useTarget.fetchTargets()
-      console.log(selectMonth.value)
-      getNumberOfMonth = optionsMonths.indexOf(selectMonth.value)
-      numberOfMonth.value = getDayInMonths(2022, getNumberOfMonth + 1)
+      refreshDaysMonths()
+      getNameofMonth()
     })
 
+
+
     return {
-      expanded: ref(false),
       numberOfMonth,
       optionsMonths,
       selectMonth,
       selectYear,
+      getNameDay,
+      refreshDaysMonths,
+      getNameofMonth,
       optionsYear,
-      caption: ref('')
     }
   },
 })
